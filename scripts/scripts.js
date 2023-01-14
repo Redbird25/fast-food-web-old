e404d0fcc96fa21e973c58ca82cc3d591a567b18
Redbird25 <122685873+Redbird25@users.noreply.github.com>
2023-01-14 20:56:04 +0500

    Add files via upload
    asd

---
+++ scripts/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        mouseDrag: false,
+        touchDrag: false,
+        pullDrag: false,
+        rewind: true,
+        autoplay: true,
+        margin: 30,
+        nav: false,
+        dots: false
+    });
+});
